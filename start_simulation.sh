3775864ee6c5c35089cbf0fa13b52831a88beaf5
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-10-20 09:55:11 +0300

    Added support for different configuration files.

--- start_simulation.sh
+++ start_simulation.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 configuration_file=$1
+platform_manager_env_file="common.env"
 
 echo "Copying the simulation configuration file to the Platform Manager."
 source copy_file_to_volume.sh $configuration_file simulation_configuration /configuration
+
+# Change the configuration file setting for Platform Manager.
+sed -i "/SIMULATION_CONFIGURATION_FILE=/c\SIMULATION_CONFIGURATION_FILE=\/configuration\/${configuration_file}" ${platform_manager_env_file}
 
 echo "Starting the Platform Manager."
 docker-compose --file docker-compose.yml up
