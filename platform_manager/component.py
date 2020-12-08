12f6e816d0e60114a41e83bca95a268ed4481fd3
Ville Heikkilä <ville.heikkila@tuni.fi>
2020-12-08 16:42:56 +0200

    Added JSON loader for component type definitions

--- platform_manager/component.py
+++ platform_manager/component.py
@@ -5,11 +5,29 @@ This module contains data classes for storing information about the components t
 """
 
 import dataclasses
+import json
 from typing import Dict, Optional
 
-CORE_COMPONENT_TYPE = "core"
-DYNAMIC_COMPONENT_TYPE = "dynamic"
-STATIC_COMPONENT_TYPE = "static"
+from tools.tools import FullLogger
+
+LOGGER = FullLogger(__name__)
+
+CORE_COMPONENT_TYPE = "core"        # there can be only one component of each "core" type within one simulation run
+DYNAMIC_COMPONENT_TYPE = "dynamic"  # a component deployed using Docker, multiple instances of each type allowed
+STATIC_COMPONENT_TYPE = "static"    # a statically deployed component, multiple instances of each type allowed
+ALLOWED_COMPONENT_TYPES = [CORE_COMPONENT_TYPE, DYNAMIC_COMPONENT_TYPE, STATIC_COMPONENT_TYPE]
+
+COMPONENT_TYPE_SIMULATION_MANAGER = "SimulationManager"
+COMPONENT_TYPE_LOG_WRITER = "LogWriter"
+
+PARAMETER_COMPONENT_TYPE = "Type"
+PARAMETER_DESCRIPTION = "Description"
+PARAMETER_DOCKER_IMAGE = "DockerImage"
+PARAMETER_ATTRIBUTES = "Attributes"
+
+ATTRIBUTE_ENVIRONMENT = "Environment"
+ATTRIBUTE_OPTIONAL = "Optional"
+ATTRIBUTE_DEFAULT = "Default"
 
 
 @dataclasses.dataclass
@@ -31,7 +49,7 @@ class ComponentAttribute:
 class ComponentParameters:
     """
     Data class for holding information about the parameters for a simulation component.
-    - component_type: the general type of the component, either "core", "dynamic" or "static"
+    - component_type: the general deployment type of the component, either "core", "dynamic" or "static"
     - description: a description for the component type
     - docker_image: a string representing the Docker image to be used with the component type,
                     should be None for a static component type
@@ -52,3 +70,47 @@ class ComponentParameters:
     include_rabbitmq_parameters: bool = True
     include_mongodb_parameters: bool = False
     include_general_parameters: bool = True
+
+
+def load_component_parameters_from_json(filename: str) -> Dict[str, ComponentParameters]:
+    """load_component_parameters_from_json"""
+    component_types = {}
+    try:
+        with open(filename, mode="r", encoding="UTF-8") as component_file:
+            component_type_definitions = json.load(component_file)
+            for component_type, component_type_definition in component_type_definitions.items():
+                deployment_type = component_type_definition.get(PARAMETER_COMPONENT_TYPE, None)
+                if deployment_type not in ALLOWED_COMPONENT_TYPES:
+                    LOGGER.warning("Component type '{}' has an unsupported deployment type: {}".format(
+                        component_type, deployment_type
+                    ))
+                    continue
+
+                # TODO: add some validation checks for the other parameters in the JSON file
+
+                component_types[component_type] = ComponentParameters(
+                    component_type=deployment_type,
+                    description=component_type_definition.get(PARAMETER_DESCRIPTION, ""),
+                    docker_image=component_type_definition.get(PARAMETER_DOCKER_IMAGE, None),
+                    attributes={
+                        attribute_name: ComponentAttribute(
+                            environment=attribute_definition.get(ATTRIBUTE_ENVIRONMENT, None),
+                            optional=attribute_definition.get(ATTRIBUTE_DEFAULT, False),
+                            default=attribute_definition.get(ATTRIBUTE_DEFAULT, None)
+                        )
+                        for attribute_name, attribute_definition in component_type_definition.get(
+                            PARAMETER_ATTRIBUTES, {}).items()
+                    },
+                    include_rabbitmq_parameters=deployment_type != STATIC_COMPONENT_TYPE,
+                    include_mongodb_parameters=component_type == COMPONENT_TYPE_LOG_WRITER,
+                    include_general_parameters=deployment_type != STATIC_COMPONENT_TYPE
+                )
+
+        LOGGER.debug("Loaded definitions for {} component types from {}".format(len(component_types), filename))
+        return component_types
+
+    except (OSError, json.decoder.JSONDecodeError) as json_error:
+        LOGGER.error("Encountered '{}' exception when loading component type definitions from '{}': {}".format(
+            str(type(json_error)), filename, json_error
+        ))
+        return {}
