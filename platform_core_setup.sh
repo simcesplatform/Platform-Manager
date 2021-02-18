b25f6239c53a0e4921bd00d85fd3375e6beea7d0
Ville Heikkilä <ville.heikkila@tuni.fi>
2021-02-18 20:35:49 +0200

    Updated platform setup script to use docker registry and manifest files.

--- platform_core_setup.sh
+++ platform_core_setup.sh
@@ -15,9 +15,18 @@ docker network create mongodb_network
 docker network create rabbitmq_network
 
 echo ""
-echo "Building the core Docker images for the simulation platform."
-docker-compose -f build/core/docker-compose-build-core.yml build
+echo "Fetching the core Docker images from Docker registry"
+docker pull procem.ain.rd.tut.fi/despa/platform_manager
+docker pull procem.ain.rd.tut.fi/despa/simulation_manager
+docker pull procem.ain.rd.tut.fi/despa/log_writer
+docker pull procem.ain.rd.tut.fi/despa/log_reader
+docker pull procem.ain.rd.tut.fi/despa/dummy
+docker pull procem.ain.rd.tut.fi/despa/manifest_fetch
 
 echo ""
 echo "Starting the background Docker containers."
 docker-compose -f background/docker-compose-background.yml up --detach
+
+echo ""
+echo "Fetching the core component manifests."
+docker-compose -f docker-compose-fetch.yml up
