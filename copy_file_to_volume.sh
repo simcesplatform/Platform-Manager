fe46920f9aaafaf0b331f78606fabea1feb9bfc9
Ville Heikkilä <ville.heikkila@tuni.fi>
2021-10-18 13:15:03 +0300

    Added helper container pre-copy-removal to file copy scripts

--- copy_file_to_volume.sh
+++ copy_file_to_volume.sh
@@ -12,9 +12,16 @@ helper_container="simces_file_copy_container"
 
 echo "Copying '$filename' to Docker volume '$volume_name' to folder '$volume_folder'"
 docker volume create $volume_name > /dev/null
+
+# remove earlier helper container
+docker stop $helper_container > /dev/null 2>&1
+docker rm $helper_container > /dev/null 2>&1
+
+# start the helper container
 docker run -d --name $helper_container --volume $volume_name:$volume_folder:rw ubuntu:18.04 sleep 10m > /dev/null
 
 docker cp $filename $helper_container:$volume_folder/$filename > /dev/null
 
+# remove the helper container
 docker stop $helper_container > /dev/null
 docker rm $helper_container > /dev/null
